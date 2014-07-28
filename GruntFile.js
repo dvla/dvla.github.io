3ce2aecf0cd049f95088dacfc82f527bb5076f69
Tony Barnes <tony@tonybarnes.me>
2014-07-28 11:27:32 +0100

    gruntjs with sass, watch

---
+++ GruntFile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+        files: {
+          'styles/styles.css' : 'styles/scss/styles.scss'
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass']
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default',['watch']);
+}
